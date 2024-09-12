f9e4ca88e8fb0b59e88d5a9594139db7ba4d31d1
harsh vekariya <patelharsh0482@gmail.com>
2024-09-12 17:33:10 +0530

    deastination page add

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,7 @@ if ($(window).width() < 1200) {
     if ($(this).next(".dropdown_list").is(':hidden')) {
       $(this).next(".dropdown_list").slideToggle('');
       $(this).toggleClass("active");
-    }  
+    }
   });
 }
 
@@ -60,13 +60,27 @@ $(document).ready(function () {
     $(this).removeClass("active");
     $('body').removeClass("body_hidden");
   });
-  //destination item
-  // $('.destination-item').click(function () {
-  //   $(this).toggleClass("active");
-  // });
   $(".destination-item").on("click", (function () {
     $(this).addClass("active").siblings().removeClass("active")
   }));
+
+  //destination page
+
+  $('.sorting-filter-wrap .tab_list_btn').on('click', function (e) {
+    if($(this).hasClass('de_list')){
+      // $('#single_card #col').addClass('col-12').removeClass('col-md-6 , col-xxl-4');
+      $('#single_card').addClass('de_list').removeClass('grid');
+      $(this).addClass('btnactive');
+      $('.grid').removeClass('btnactive');
+      
+    }
+    else if ($(this).hasClass('grid')) {
+      // $('#single_card #col').addClass('col-md-6 , col-xxl-4').removeClass('col-12');
+      $('#single_card').addClass('grid').removeClass('de_list');
+      $(this).addClass('btnactive');
+      $('.de_list').removeClass('btnactive');
+    }
+  });
 });
 $(document).on("click", function (event) {
 
@@ -94,225 +108,11 @@ $(document).on("click", function (event) {
 
 });
 
-//slider first
-let swiper = new Swiper(".thumbswiper", {
-  loop: false,
-  spaceBetween: 10,
-  slidesPerView: 2,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-let swiper2 = new Swiper(".mySwiper2", {
-  pagination: {
-    el: ".swiper-pagination"
-  },
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  autoplay: {
-    delay: 4000,
-    speed: 800,
-    disableOnInteraction: false,
-  },
-  thumbs: {
-    swiper: swiper,
-  },
-});
-
-
-// slider tour-categorie
-const swiper1 = new Swiper(".tour-swiper", {
-  // slidesPerView: 5,
-  // spaceBetween: 60,
-  // loop: true,
-  // grabCursor: true,
-  // centeredSlides: true,
-  // slideActiveClass: "active",
-  // pagination: {
-  //   el: ".pagination",
-  //   clickable: true
-  // },
-  slidesPerView: "auto",
-  slidesPerView: 5,
-  spaceBetween: 60,
-  centeredSlides: !0,
-  loop: !0,
-  grabCursor: !0,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: !0
-  },
-  breakpoints: {
-    200: {
-      slidesPerView: 1,
-      spaceBetween: 30
-    },
-    576: {
-      slidesPerView: 2,
-      spaceBetween: 30
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 30
-    },
-    1024: {
-      slidesPerView: 4,
-      spaceBetween: 40
-    },
-    1280: {
-      slidesPerView: 5,
-      spaceBetween: 60
-    }
-  }
-});
-// slider meet-guide-swiper
-const swiper3 = new Swiper(".meet-guide-swiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: !0
-  },
-  autoplay: {
-    delay: 4000,
-    speed: 800,
-  },
-  breakpoints: {
-    200: {
-      slidesPerView: 1,
-      spaceBetween: 30
-    },
-    576: {
-      slidesPerView: 1,
-      // spaceBetween: 30
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 30
-    },
-    991: {
-      slidesPerView: 3,
-      spaceBetween: 30
-    },
-    1199: {
-      slidesPerView: 4,
-      spaceBetween: 30
-    }
-  }
-});
-
 // offer-deals
 $('.offer-deals .accordion-tab-item').hover(function () {
   var value = $(this).attr('data-src');
   $('#card-cover').attr('src', value);
 });
-
-// popular-destination-swiper
-var popular_destination_swiper = new Swiper(".popular-destination-swiper", {
-  slidesPerView: 3,
-  spaceBetween: 24,
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    576: {
-      slidesPerView: 1,
-    },
-    767: {
-      slidesPerView: 2,
-    },
-    1199: {
-      slidesPerView: 3,
-    },
-    1400: {
-      slidesPerView: 4,
-    }
-  }
-});
-
-// company-swiper
-const company_swiper = new Swiper(".company-swiper", {
-  slidesPerView: 5,
-  spaceBetween: 30,
-  freeMode: true,
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    576: {
-      slidesPerView: 2,
-    },
-    767: {
-      slidesPerView: 3,
-    },
-    991: {
-      slidesPerView: 4,
-    },
-    1199: {
-      slidesPerView: 6,
-    },
-    1400: {
-      slidesPerView: 8,
-    }
-  }
-});
-// company-swiper
-const blog_swiper = new Swiper(".blog-swiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  freeMode: true,
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    767: {
-      slidesPerView: 2,
-    },
-    1199: {
-      slidesPerView: 3,
-    }
-  }
-});
-
-// Testimonial
-const testimonial = new Swiper(".testimonial-slider", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-  },
-  spaceBetween: 40,
-  loop: true,
-  breakpoints: {
-    640: {
-      slidesPerView: 2
-    },
-    1024: {
-      slidesPerView: 2
-    }
-  }
-});
-
 
 // counter
 $('.counter-number').each(function () {
@@ -325,18 +125,6 @@ $('.counter-number').each(function () {
       $(this).text(Math.ceil(now));
     }
   });
-}); 
+});
 
-// //animation
-// sal({
-//   'threshold': 0.01,
-//   once: false
-// });
 
-// Fancybox.bind("[data-fancybox]", {
-//   // Your custom options
-// });
-// Fancybox.bind('[data-fancybox="gallery1"]', {
-//   Thumbs: false,
-//   Toolbar: false
-// });
---
+++ assets/js/slider.js
@@ -0,0 +1,209 @@
+
+//slider first
+let swiper = new Swiper(".thumbswiper", {
+    loop: false,
+    spaceBetween: 10,
+    slidesPerView: 2,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+let swiper2 = new Swiper(".mySwiper2", {
+    pagination: {
+        el: ".swiper-pagination"
+    },
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 4000,
+        speed: 800,
+        disableOnInteraction: false,
+    },
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
+
+// slider tour-categorie
+const swiper1 = new Swiper(".tour-swiper", {
+    slidesPerView: "auto",
+    slidesPerView: 5,
+    spaceBetween: 60,
+    centeredSlides: !0,
+    loop: !0,
+    grabCursor: !0,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: !0
+    },
+    breakpoints: {
+        200: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        },
+        1280: {
+            slidesPerView: 5,
+            spaceBetween: 60
+        }
+    }
+});
+// slider meet-guide-swiper
+const swiper3 = new Swiper(".meet-guide-swiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    speed: 1000,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: !0
+    },
+    autoplay: {
+        delay: 4000,
+        speed: 800,
+    },
+    breakpoints: {
+        200: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        576: {
+            slidesPerView: 1,
+            // spaceBetween: 30
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1199: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        }
+    }
+});
+
+// popular-destination-swiper
+var popular_destination_swiper = new Swiper(".popular-destination-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 24,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        576: {
+            slidesPerView: 1,
+        },
+        767: {
+            slidesPerView: 2,
+        },
+        1199: {
+            slidesPerView: 3,
+        },
+        1400: {
+            slidesPerView: 4,
+        }
+    }
+});
+
+// company-swiper
+const company_swiper = new Swiper(".company-swiper", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        576: {
+            slidesPerView: 2,
+        },
+        767: {
+            slidesPerView: 3,
+        },
+        991: {
+            slidesPerView: 4,
+        },
+        1199: {
+            slidesPerView: 6,
+        },
+        1400: {
+            slidesPerView: 8,
+        }
+    }
+});
+// company-swiper
+const blog_swiper = new Swiper(".blog-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        767: {
+            slidesPerView: 2,
+        },
+        1199: {
+            slidesPerView: 3,
+        }
+    }
+});
+
+// Testimonial
+const swiper4 = new Swiper(".testi_monial", {
+    slidesPerView: 2,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 30,
+    // freeMode: true,
+    speed: 2000,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        767: {
+            slidesPerView: 2,
+        }
+    }
+});
